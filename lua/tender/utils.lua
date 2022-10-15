9404cdad71bf8bab1a64655ad46e284ac3fa94ab
Martin Roa Villescas <mroavi@gmail.com>
2022-10-15 19:50:47 +0200

    Add TODO note to use lua's API for setting highlights

--- lua/tender/utils.lua
+++ lua/tender/utils.lua
@@ -6,6 +6,8 @@ function utils.highlight(group, color)
   local bg = color.bg and "guibg=" .. color.bg or "guibg=NONE"
   local sp = color.sp and "guisp=" .. color.sp or ""
   vim.api.nvim_command("highlight " .. group .. " " .. style .. " " .. fg .. " " .. bg .. " " .. sp)
+  -- TODO: set highlight group with lua. E.g.:
+  --vim.api.nvim_set_hl(0, 'Normal', { fg = "#ffffff", bg = "#333333" })
 end
 
 return utils
